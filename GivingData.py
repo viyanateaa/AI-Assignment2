af7ea042ab7ed42507443235342b9d595b034146
viyanateaa <viyanateaa@gmail.com>
2017-09-26 21:02:12 +0200

    create 2d array, not done yet

--- GivingData.py
+++ GivingData.py
@@ -14,7 +14,11 @@ class GivingData:
 
 
         with open(file_name) as txt_file:
-            data = []
+            #data = [][]
+            data = [[0 for i in range(19)] for j in range(19)]
+            # Creates a list containing 5 lists, each of 8 items, all set to 0
+           # w, h = 8, 5;
+            #Matrix = [[0 for x in range(w)] for y in range(h)]
            # lines = txt_file.read().split('\n')
 
             for line in txt_file:
